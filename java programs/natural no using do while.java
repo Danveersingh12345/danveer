ef179b3a2474083f045766b410ff6f38c395720e
Danveersingh12345 <122141910+Danveersingh12345@users.noreply.github.com>
2023-08-08 09:00:27 +0530

    Add files via upload

---
+++ java programs/natural no using do while.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class naturalno{
+    public static void main(String[] args){
+        int i=1;
+        Scanner Sc =new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER");
+        int n=Sc.nextInt();
+        
+        do{
+            System.out.println(i);
+            i++;
+            
+        }
+            while(i<=n);
+            
+        
+    }
+}
